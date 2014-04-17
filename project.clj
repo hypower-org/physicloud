eca8e45e220494a72db4c0e3a40f47b8ebc4803c
Patrick Martin <prof.pmartin10@gmail.com>
2014-04-17 09:14:10 -0400

    Cleaned out unnecessary project dependencies in the project file.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject PhysiCloud "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "PhysiCloud: A software platform to facilitate programming on cyber-physical systems."
+  :url "http://github.com/hypower-org/physicloud"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main  actor-development-tests.systemfourtest
@@ -10,15 +10,12 @@
                  [lamina "0.5.2"]
                  [lacij "0.8.1"]
                  [rhizome "0.1.9"]
-                 [bifocals "0.0.2"]
                  [aleph "0.3.2"]
                  [gloss "0.2.2"]
                  [criterium "0.4.3"]
                  [seesaw "1.4.4"]
                  [org.clojure/data.csv "0.1.2"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
-                 [net.mikera/core.matrix "0.13.1"]
-                 [net.mikera/vectorz-clj "0.15.0"]
                  [incanter/incanter-core "1.5.4"]
                  [net.async/async "0.1.0"]
                  [incanter/incanter-charts "1.5.4"]
