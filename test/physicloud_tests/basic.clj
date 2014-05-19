fa5d3d60aadda3cfea6aa49163d359c8a407c40a
Patrick Martin <pjmartin@ycp.edu>
2014-05-19 14:30:30 -0400

    Started adding simple functions to the basic test file.

--- test/physicloud_tests/basic.clj
+++ test/physicloud_tests/basic.clj
@@ -8,3 +8,12 @@
 (def test-cpu (core/cyber-physical-unit "10.10.10.5"))
 
 (core/on-pool t/exec (core/into-physicloud test-cpu :heartbeat 5000 :on-disconnect (fn [] (println "Disconnected!"))))
+
+; A couple simple functions that perform silly tasks:
+(defn producer
+  [&]
+  {:producer "42"})
+
+(defn consumer 
+  [input-map] 
+  (vector (str (keys input-map)) (str (vals input-map))))
