3a3a5ee0153edf1c137b2bf19f601c9c3ba03658
Patrick Martin <pjmartin@ycp.edu>
2014-06-03 14:58:09 -0400

    Customized the consumer.clj file for remote testing and bug hunting.

--- test/physicloud_tests/consumer.clj
+++ test/physicloud_tests/consumer.clj
@@ -1,25 +1,21 @@
 (ns physicloud-tests.consumer
  (:require [physicloud.core :as core]
-            [physicloud.task :as t])
- (:gen-class))
+            [physicloud.task :as t]
+            [lamina.core :as lamina])
+ ;(:gen-class)
+ )
 ; This test demonstrates the creation of a basic Cyber Physical Unit in PhysiCloud.
 (defn -main [x]
-; Create a CPU at the provided IP address.
-(let [test-cpu2 (core/cyber-physical-unit (str x))]
+  (def consumer-cpu (core/cyber-physical-unit (str x)))
+  
+  (def all-channels (:total-channel-list consumer-cpu))
+  
+  ;(lamina/enqueue (:network-out-channel @all-channels) "kernel|{:hi 1}")
 
-(defn producerfn []
-  (println "Producer producing...") {:producer "42"})
-
-(core/on-pool t/exec (core/into-physicloud test-cpu2 :heartbeat 5000 :on-disconnect (fn [] (println "Disconnected!"))))
-
-;(core/task test-cpu2 {:name "producer"
-;                     :function (fn [this] (producerfn))
-;                     :produces "awesome-data-map"
-;                     :update-time 2000
-;                     })
-
-(core/task test-cpu2 {:name "consumer"
-                      :function (fn [this awesome-data-map]
-                                  (println (vector (str (keys awesome-data-map)) (str (vals awesome-data-map)))))
-                      })
-))
+  ; :network-out-channel
+   
+;(core/task test-cpu2 {:name "consumer"
+;                      :function (fn [this awesome-data-map]
+;                                  (println (vector (str (keys awesome-data-map)) (str (vals awesome-data-map)))))
+;                      })
+)
