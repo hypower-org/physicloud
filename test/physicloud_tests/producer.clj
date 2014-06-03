62e3f37a4ec48de49bcdb4811a468c3a9972c936
snelso15 <snelso15@ycp.edu>
2014-06-03 13:43:48 -0400

    Added the producer and consumer namespaces.

---
+++ test/physicloud_tests/producer.clj
@@ -0,0 +1,35 @@
+(ns physicloud-tests.producer
+ (:require [physicloud.core :as core]
+            [physicloud.task :as t]
+            [lamina.core :as lamina])
+  (:gen-class))
+
+(def test-cpu (core/cyber-physical-unit "10.10.10.5"))
+ 
+(defn  -main [x]
+ ;Create a CPU at the provided IP address.
+(let [
+      
+      ;test-cpu (core/cyber-physical-unit (str x))
+      
+      ]
+  
+  (lamina/receive-all (:kernel @(:total-channel-list test-cpu)) println)
+
+(defn producerfn []
+  (println "Producer producing...") {:producer "42"})
+
+(core/on-pool t/exec (core/into-physicloud test-cpu :heartbeat 5000 :on-disconnect (fn [] (println "Disconnected!"))))
+
+(core/task test-cpu {:name "producer"
+                     :function (fn [this] (producerfn))
+                     :produces "awesome-data-map"
+                     :update-time 2000
+                     })
+
+;(core/task test-cpu {:name "consumer"
+;                     :function (fn [this awesome-data-map]
+;                                 (println (vector (str (keys awesome-data-map)) (str (vals awesome-data-map)))))
+;                     })
+
+))
