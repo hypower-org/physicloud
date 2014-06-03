6846d6b2b3bcb384a0a4cf2af1ca7e04648315f8
Patrick Martin <pjmartin@ycp.edu>
2014-06-03 15:16:47 -0400

    Hard coded the process for starting the consumer on a separate IP. It works.

--- test/physicloud_tests/consumer.clj
+++ test/physicloud_tests/consumer.clj
@@ -4,18 +4,17 @@
             [lamina.core :as lamina])
  ;(:gen-class)
  )
-; This test demonstrates the creation of a basic Cyber Physical Unit in PhysiCloud.
-(defn -main [x]
-  (def consumer-cpu (core/cyber-physical-unit (str x)))
-  
-  (def all-channels (:total-channel-list consumer-cpu))
-  
-  ;(lamina/enqueue (:network-out-channel @all-channels) "kernel|{:hi 1}")
 
-  ; :network-out-channel
-   
-;(core/task test-cpu2 {:name "consumer"
-;                      :function (fn [this awesome-data-map]
-;                                  (println (vector (str (keys awesome-data-map)) (str (vals awesome-data-map)))))
-;                      })
-)
+(defn -main [x]
+ (def consumer-cpu (core/cyber-physical-unit (str x)))
+ 
+ (core/instruction consumer-cpu [core/START-TCP-CLIENT "10.10.10.5" 8998])
+ 
+;  (def all-channels (:total-channel-list consumer-cpu))
+;  (lamina/enqueue (:network-out-channel @all-channels) "kernel|{:hi 1}")
+ 
+ (core/task consumer-cpu {:name "consumer"
+                    :function (fn [this awesome-data-map]
+                                (println (vector (str (keys awesome-data-map)) (str (vals awesome-data-map)))))
+                    })
+ )
