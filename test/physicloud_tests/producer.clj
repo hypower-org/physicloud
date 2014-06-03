4e9151cdac2d8ff2590e5845362247bedfdcfaa4
pglotfel <pglotfel@ycp.edu>
2014-06-03 14:55:24 -0400

    Bug fix for ping functionality.

--- test/physicloud_tests/producer.clj
+++ test/physicloud_tests/producer.clj
@@ -4,32 +4,30 @@
             [lamina.core :as lamina])
   (:gen-class))
 
-(def test-cpu (core/cyber-physical-unit "10.10.10.5"))
- 
 (defn  -main [x]
- ;Create a CPU at the provided IP address.
-(let [
-      
-      ;test-cpu (core/cyber-physical-unit (str x))
-      
-      ]
+ (def test-cpu (core/cyber-physical-unit (str x)))
   
-  (lamina/receive-all (:kernel @(:total-channel-list test-cpu)) println)
+ (lamina/receive-all (:kernel @(:total-channel-list test-cpu)) println)
+ 
+ 
+  
+ (def server (lamina/wait-for-message (core/instruction test-cpu [core/START-SERVER 8998])))
+ (core/instruction test-cpu [core/START-TCP-CLIENT "10.10.10.5" 8998]))
+ 
+; (lamina/receive-all (:kernel @(:total-channel-list test-cpu)) println)
 
-(defn producerfn []
-  (println "Producer producing...") {:producer "42"})
+;(defn producerfn []
+;  (println "Producer producing...") {:producer "42"})
+;)
 
-(core/on-pool t/exec (core/into-physicloud test-cpu :heartbeat 5000 :on-disconnect (fn [] (println "Disconnected!"))))
 
-(core/task test-cpu {:name "producer"
-                     :function (fn [this] (producerfn))
-                     :produces "awesome-data-map"
-                     :update-time 2000
-                     })
 
-;(core/task test-cpu {:name "consumer"
-;                     :function (fn [this awesome-data-map]
-;                                 (println (vector (str (keys awesome-data-map)) (str (vals awesome-data-map)))))
-;                     })
 
-))
+;(core/on-pool t/exec (core/into-physicloud test-cpu :heartbeat 5000 :on-disconnect (fn [] (println "Disconnected!"))))
+
+;(core/task test-cpu {:name "producer"
+;                     :function (fn [this] (producerfn))
+;                     :produces "awesome-data-map"
+;                     :update-time 2000
+;                     }))
+
