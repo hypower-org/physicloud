661c128e7b75ae21e46fc8b2655cf551bc92ee8c
snelso15 <snelso15@ycp.edu>
2014-06-05 11:28:37 -0400

    Added robot producer namespace to handle the key presses.

---
+++ test/physicloud_tests/robot_producer.clj
@@ -0,0 +1,17 @@
+(ns physicloud-tests.robot-producer
+  (use seesaw.core)
+  (:require   [physicloud.core :as core]
+              [physicloud.task :as t]
+              [physicloud-tests.sst :as sst]))
+  
+(def robot-producer-cpu (core/cyber-physical-unit "10.10.10.5"))
+
+(core/on-pool t/exec (core/into-physicloud robot-producer-cpu :heartbeat 5000 :on-disconnect (fn [] (println "Disconnected!"))))
+
+(-> sst/myframe pack!
+                show!)
+
+(core/task robot-producer-cpu {:name "producer"
+                               :function (fn [this] (sst/producerfn))
+                               :produces "robot-control-data"
+                               :update-time 2000})
