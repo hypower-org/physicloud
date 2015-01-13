661864795224be5cb8b86c849f70538ad5a29bde
Patrick Martin <prof.pmartin10@gmail.com>
2015-01-13 14:09:46 -0500

    Set up a release for physicloud to clojars.

--- project.clj
+++ project.clj
@@ -1,26 +1,18 @@
-(defproject physicloud "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject hypower-org/physicloud "0.1.0"
+  :description "A platform for programming and managing cyber-physical systems (CPS)."
+  :url "http://github.com/hypower-org/physicloud"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [incanter "1.5.5"]
                  [net.mikera/core.matrix "0.29.1"]
-                 [manifold "0.1.0-beta5"]
-                 ;[org.clojure/core.async "0.1.346.0-17112a-alpha"]
+                 [manifold "0.1.0-beta7"]
                  [criterium "0.4.3"]
-                 ;[seesaw "1.4.4"]
-                 [watershed "0.1.0"]
+                 [hypower-org/watershed "0.1.3"]
                  [org.clojure/data.int-map "0.1.0"]
                  [com.taoensso/nippy "2.7.0"]
                  [org.clojure/core.match "0.2.1"]
                  [byte-streams "0.2.0-alpha4"]
                  [org.clojure/math.numeric-tower "0.0.4"]
-                 [gloss "0.2.4-SNAPSHOT"]
-                 [aleph "0.4.0-alpha9"]]
-                 ;[org.scream3r/jssc "2.8.0"]
-                 ;[jkobuki "1.1.0"]
-  :plugins [[lein-nodisassemble "0.1.3"]]
-  ;:main physicloud.kobuki-gt-agent
-  ;:aot [physicloud.kobuki-gt-agent]
-  )
+                 [gloss "0.2.4"]
+                 [aleph "0.4.0-alpha9"]])
