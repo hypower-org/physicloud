9de7d6480e1116dad5409ebee356fe3d4ccb38a7
Patrick Martin <prof.pmartin10@gmail.com>
2015-01-28 12:28:10 -0500

    Changed the calls to watershed/outline to watershed/vertex to match up with the new api names.

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [hypower-org/watershed "0.1.3"]
+                 [hypower-org/watershed "0.1.4"]
                  [aleph "0.4.0-alpha9"]
                  [incanter "1.5.5"]
                  [net.mikera/core.matrix "0.29.1"]
--- test/physicloud/simple_feedback_example.clj
+++ test/physicloud/simple_feedback_example.clj
@@ -6,22 +6,23 @@
 
 ; This file shows how to construct PhysiCloud application with a feedback loop.
 
-(let [reference (w/outline :ref [] ; Declared the reference input waterway. It has no dependencies.
-                           (fn []
-                             (s/periodically 500 (fn [] 1)))) ; Generate a reference input of 1 every 500 ms.
+(def simple-feedback-instance
+  (let [reference (w/vertex :ref [] ; Declared the reference input waterway. It has no dependencies.
+                            (fn []
+                              (s/periodically 1000 (fn [] 1)))) ; Generate a reference input of 1 every 500 ms.
       
-      plant (w/outline :plant-output [:plant-output :ref] ; Make a plant and have it require its own information and the reference.
-                       (fn 
-                         ([] 0) ; Initial condition for :plant-output, since we have an information cycle.
-                         ([& streams] ; After physicloud starts we get data from the :plant-output and :ref streams for processing. 
-                           (s/map (fn [[output ref]]
-                                    (+ output ref)) ; positive feedback - not stable :o
-                                  (apply s/zip streams))) ; interleaves the data from the output and ref streams into a vector
-                         ))
+       plant (w/vertex :plant-output [:plant-output :ref] ; Make a plant and have it require its own information and the reference.
+                        (fn 
+                          ([] 0) ; Initial condition for :plant-output, since we have an information cycle.
+                          ([& streams] ; After physicloud starts we get data from the :plant-output and :ref streams for processing. 
+                            (s/map (fn [[output ref]]
+                                     (+ output ref)) ; positive feedback - not stable :o
+                                   (apply s/zip streams))) ; interleaves the data from the output and ref streams into a vector
+                          ))
       
-      printer (w/outline :printer [:plant-output] ; Just a sink that prints the result of the feedback loop.
-                         (fn [stream] (s/consume (fn [val] (println "output = " val))
-                                                 ; identity copies and preserves the :plant-output stream, so that data is not destructively removed.
-                                                 (s/map identity stream)))) 
-      ]
-  (phy/assemble-phy reference plant printer)) ; build and run the system!
+       printer (w/vertex :printer [:plant-output] ; Just a sink that prints the result of the feedback loop.
+                          (fn [stream] (s/consume (fn [val] (println "output = " val))
+                                                  ; identity copies and preserves the :plant-output stream, so that data is not destructively removed.
+                                                  (s/map identity stream)))) 
+       ]
+   (phy/assemble-phy reference plant printer))) ; build and run the system!
